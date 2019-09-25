2d27abf531ee2155fac9c29bb162690af6a97fd1
nbpeth <nick.peth@returnpath.com>
2019-09-25 14:43:44 -0600

    update message format

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -20,49 +20,86 @@ export class DashboardComponent implements OnInit {
       .getSocket()
       .subscribe(this.handleEvents);
 
-    this.socketService.send({[Events.COMPLETE_STATE]: 'undefined'});
+    // this.socketService.send({[Events.COMPLETE_STATE]: 'undefined'});
+    this.socketService.send(new GetCompleteStateMessage());
   }
 
   createNewSession = (name: string) => {
-    const maybeExistsAlready = this.activeSessions.find(session => session.id === name);
-
-    if (maybeExistsAlready) {
-      this.error = 'Session Name Already Exists';
-      return;
-    }
+    // const maybeExistsAlready = this.activeSessions.find(session => session.id === name);
+    //
+    // if (maybeExistsAlready) {
+    //   this.error = 'Session Name Already Exists';
+    //   return;
+    // }
 
     const defaultName = `Session${Math.floor(Math.random() * 100000)}`;
-    this.socketService.send({[Events.SESSION_CREATED]: {sessionName: name ? name : defaultName}});
+    const newSessionName = name ? name : defaultName;
+    console.log("NAME", newSessionName, "...", name, defaultName)
+    const message = new CreateNewSessionMessage(new NewSessionPayload(newSessionName));
+
+    this.socketService.send(message);
+
   };
 
-  private handleEvents = (event) => {
-    console.log('received::', event);
+  private handleEvents = (message: MessageEvent) => {
+    console.log('received::', message);
 
-    const json = JSON.parse(event.data);
-    const eventKey = Object.keys(json)[0];
-    const data = Object.values(json)[0];
+    const messageData = JSON.parse(message.data) as SpMessage;
+    const eventType = messageData.eventType;
+    const payload = messageData.payload;
 
-    switch (eventKey) {
-      case Events.SESSION_CREATED:
-        this.setSessionsFrom(data);
-        break;
+    switch (eventType) {
       case Events.COMPLETE_STATE:
-        this.setSessionsFrom(data);
+        console.log('GIMME DAT STATE!', eventType, payload);
         break;
-      default:
     }
+
+
   };
 
   private setSessionsFrom = (data: any) => {
-    const allActiveSessions = data.sessions;
-    return this.activeSessions = Object.entries(allActiveSessions).map(this.mapActiveSessions);
+    // const allActiveSessions = data.sessions;
+    // return this.activeSessions = Object.entries(allActiveSessions).map(this.mapActiveSessions);
   };
 
   private mapActiveSessions = (sessionInfo: any): Session => {
     const activeSession = new Session();
-    activeSession.id = sessionInfo[0] as string;
-    activeSession.sessionState = sessionInfo[1];
+    // activeSession.id = sessionInfo[0] as string;
+    // activeSession.sessionState = sessionInfo[1];
 
     return activeSession;
   };
+}
+
+export class SpMessage {
+  eventType: string;
+  payload: SpMessagePayload;
+
+  constructor(payload?: SpMessagePayload) {
+    this.payload = payload;
+  };
+
+}
+
+export class SpMessagePayload {
+}
+
+export class NewSessionPayload extends SpMessagePayload {
+  constructor(public sessionName: string) {
+    super();
+  }
+}
+
+export class GetCompleteStateMessage extends SpMessage {
+  constructor() {
+    super();
+    this.eventType = Events.COMPLETE_STATE as string;
+  }
+}
+
+export class CreateNewSessionMessage extends SpMessage {
+  constructor(payload: SpMessagePayload) {
+    super(payload);
+    this.eventType = Events.SESSION_CREATED as string;
+  }
 }
