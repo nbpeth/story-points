ee73f2f1908e8a04534e8d25e20c3fad99299bbc
nbpeth <nick.peth@returnpath.com>
2019-11-18 16:18:12 -0700

    update terraform name

--- ops/terraform/main.tf
+++ ops/terraform/main.tf
@@ -1,5 +1,5 @@
-resource "aws_ecr_repository" "story-points" {
-  name = "story-points"
+resource "aws_ecr_repository" "rp-story-points" {
+  name = "rp-story-points"
 
   image_scanning_configuration {
     scan_on_push = true
