fee100c885e63fcbeb6ddeeaa499343de67a9fda
Nicholas Peth <nbpeth@Nicholass-iMac.local>
2019-12-02 12:03:29 -0700

    connect to local mongo

--- server.js
+++ server.js
@@ -4,200 +4,206 @@ const path = require('path');
 const cors = require('cors');
 const app = express();
 const WebSocketServer = require("ws").Server;
-/*
-  serve static UI content
- */
-app.use(express.static(__dirname + '/dist/story-points'));
-
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/story-points/index.html'));
-});
-
-/*
-  web socket server
- */
-
-
-const http = require('http');
-
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-app.use(cors);
-
-const server = http.createServer(app);
-const wss = new WebSocketServer({server: server, path: "/socket"});
+const dbClient = require('./db');
 
 let _ws;
 const state = {
   sessions: {},
 };
 
-handleNewClients = (ws) => {
-  _ws = ws;
-  const message = formatMessage('state-of-the-state', state);
+const initHandlers = (db) => {
+  /*
+  serve static UI content
+ */
+  app.use(express.static(__dirname + '/dist/story-points'));
 
-  notifyCaller(message);
-
-  ws.on('message', handleIncomingMessages);
-};
-
-handleIncomingMessages = (message) => {
-  const messageData = JSON.parse(message);
-  const eventType = messageData.eventType;
-
-  console.log(messageData);
-  console.log('');
-
-  switch (eventType) {
-    case 'state-of-the-state':
-      notifyCaller(formatMessage(eventType, state));
-      break;
-    case 'session-created':
-      createNewSession(messageData);
-      break;
-    case 'session-state':
-      getSessionState(messageData);
-      break;
-    case 'participant-joined':
-      addParticipantToSession(messageData);
-      break;
-    case 'participant-removed':
-      removeParticipantFromSession(messageData);
-      break;
-    case 'point-submitted':
-      pointWasSubmitted(messageData);
-      break;
-    case 'points-reset':
-      resetPoints(messageData);
-      break;
-    case 'points-revealed':
-      revealPoints(messageData);
-      break;
-    case 'terminate-session':
-      terminateSession(messageData);
-      break;
-
-  }
-};
-
-// protect terminated sessions from being able to take actions
-
-terminateSession = (messageData) => {
-  const payload = messageData.payload;
-  const requestedSession = payload.sessionName;
-
-  delete state.sessions[requestedSession];
-
-  const message = formatMessage('state-of-the-state', state);
-  notifyClients(message);
-};
-
-revealPoints = (messageData) => {
-  const eventType = messageData.eventType;
-  const payload = messageData.payload;
-  const requestedSession = payload.sessionName;
-  const sessionData = state.sessions[requestedSession];
-  notifyClients(formatMessage(eventType, sessionData, requestedSession))
-};
-
-resetPoints = (messageData) => {
-  const eventType = messageData.eventType;
-  const payload = messageData.payload;
-  const requestedSession = payload.sessionName;
-  const sessionData = state.sessions[requestedSession];
-
-  Object.values(sessionData).forEach(participant => {
-    Object.values(participant).forEach(p => {
-      if (p) {
-        p['hasVoted'] = false;
-        p.point = 0;
-      }
-    });
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/dist/story-points/index.html'));
   });
 
-  notifyClients(formatMessage(eventType, sessionData, requestedSession))
+  /*
+    web socket server
+   */
 
-};
+  const http = require('http');
 
-getSessionState = (messageData) => {
-  const eventType = messageData.eventType;
-  const payload = messageData.payload;
-  const requestedSession = payload.sessionName;
-  const sessionData = state.sessions[requestedSession];
-  notifyCaller(formatMessage(eventType, sessionData, requestedSession));
-};
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+  app.use(cors);
 
-pointWasSubmitted = (messageData) => {
-  const payload = messageData.payload;
-  const requestedSession = payload.sessionName;
-  const sessionData = state.sessions[requestedSession];
-  const targetUser = payload.userName;
+  const server = http.createServer(app);
+  const wss = new WebSocketServer({ server: server, path: "/socket" });
 
-  const userData = sessionData.participants[targetUser];
-  userData.point = payload.value;
-  userData.hasVoted = true;
-  notifyClients(formatMessage(messageData.eventType, sessionData, requestedSession))
-};
+  handleNewClients = (ws) => {
+    _ws = ws;
+    const message = formatMessage('state-of-the-state', state);
 
-addParticipantToSession = (messageData) => {
-  const payload = messageData.payload;
-  const requestedSession = payload.sessionName;
-  const sessionData = state.sessions[requestedSession];
-  const participantToAdd = payload.userName;
-  const isAdmin = payload.isAdmin;
+    notifyCaller(message);
 
-  sessionData.participants[participantToAdd] = {point: 0, isAdmin: isAdmin};
+    ws.on('message', handleIncomingMessages);
+  };
 
-  notifyClients(formatMessage(messageData.eventType, sessionData, requestedSession))
-};
+  handleIncomingMessages = (message) => {
+    const messageData = JSON.parse(message);
+    const eventType = messageData.eventType;
 
-removeParticipantFromSession = (messageData) => {
-  const payload = messageData.payload;
-  const requestedSession = payload.sessionName;
-  const sessionData = state.sessions[requestedSession];
-  const participantToRemove = payload.userName;
+    console.log(messageData);
+    console.log('');
 
-  sessionData.participants[participantToRemove] = undefined;
+    switch (eventType) {
+      case 'state-of-the-state':
+        notifyCaller(formatMessage(eventType, state));
+        break;
+      case 'session-created':
+        createNewSession(messageData);
+        break;
+      case 'session-state':
+        getSessionState(messageData);
+        break;
+      case 'participant-joined':
+        addParticipantToSession(messageData);
+        break;
+      case 'participant-removed':
+        removeParticipantFromSession(messageData);
+        break;
+      case 'point-submitted':
+        pointWasSubmitted(messageData);
+        break;
+      case 'points-reset':
+        resetPoints(messageData);
+        break;
+      case 'points-revealed':
+        revealPoints(messageData);
+        break;
+      case 'terminate-session':
+        terminateSession(messageData);
+        break;
 
-  notifyClients(formatMessage(messageData.eventType, sessionData, requestedSession))
-};
+    }
+  };
 
-createNewSession = (messageData) => {
-  const payload = messageData.payload;
-  const sessionName = payload && payload.sessionName ? payload.sessionName : undefined;
-  if (!sessionName) {
-    return;
-  }
-  state.sessions[sessionName] = {participants: {}};
-  const stateMessage = formatMessage('session-created', state);
+  terminateSession = (messageData) => {
+    const payload = messageData.payload;
+    const requestedSession = payload.sessionName;
 
-  notifyClients(stateMessage)
-};
+    delete state.sessions[requestedSession];
 
-formatMessage = (eventType, payload, targetSession) => ({
-  eventType: eventType,
-  payload: payload,
-  targetSession: targetSession,
-});
+    const message = formatMessage('state-of-the-state', state);
+    notifyClients(message);
+  };
 
-notifyClients = (message) => {
-  wss.clients
-    .forEach(client => {
-      client.send(JSON.stringify(message));
+  revealPoints = (messageData) => {
+    const eventType = messageData.eventType;
+    const payload = messageData.payload;
+    const requestedSession = payload.sessionName;
+    const sessionData = state.sessions[requestedSession];
+    notifyClients(formatMessage(eventType, sessionData, requestedSession))
+  };
+
+  resetPoints = (messageData) => {
+    const eventType = messageData.eventType;
+    const payload = messageData.payload;
+    const requestedSession = payload.sessionName;
+    const sessionData = state.sessions[requestedSession];
+
+    Object.values(sessionData).forEach(participant => {
+      Object.values(participant).forEach(p => {
+        if (p) {
+          p['hasVoted'] = false;
+          p.point = 0;
+        }
+      });
     });
-};
 
-notifyCaller = (message) => {
-  wss.clients
-    .forEach(client => {
-      if (client === _ws) {
+    notifyClients(formatMessage(eventType, sessionData, requestedSession))
+
+  };
+
+  getSessionState = (messageData) => {
+    const eventType = messageData.eventType;
+    const payload = messageData.payload;
+    const requestedSession = payload.sessionName;
+    const sessionData = state.sessions[requestedSession];
+    notifyCaller(formatMessage(eventType, sessionData, requestedSession));
+  };
+
+  pointWasSubmitted = (messageData) => {
+    const payload = messageData.payload;
+    const requestedSession = payload.sessionName;
+    const sessionData = state.sessions[requestedSession];
+    const targetUser = payload.userName;
+
+    const userData = sessionData.participants[targetUser];
+    userData.point = payload.value;
+    userData.hasVoted = true;
+    notifyClients(formatMessage(messageData.eventType, sessionData, requestedSession))
+  };
+
+  addParticipantToSession = (messageData) => {
+    const payload = messageData.payload;
+    const requestedSession = payload.sessionName;
+    const sessionData = state.sessions[requestedSession];
+    const participantToAdd = payload.userName;
+    const isAdmin = payload.isAdmin;
+
+    sessionData.participants[participantToAdd] = { point: 0, isAdmin: isAdmin };
+
+    notifyClients(formatMessage(messageData.eventType, sessionData, requestedSession))
+  };
+
+  removeParticipantFromSession = (messageData) => {
+    const payload = messageData.payload;
+    const requestedSession = payload.sessionName;
+    const sessionData = state.sessions[requestedSession];
+    const participantToRemove = payload.userName;
+
+    sessionData.participants[participantToRemove] = undefined;
+
+    notifyClients(formatMessage(messageData.eventType, sessionData, requestedSession))
+  };
+
+  createNewSession = (messageData) => {
+    const payload = messageData.payload;
+    const sessionName = payload && payload.sessionName ? payload.sessionName : undefined;
+    if (!sessionName) {
+      return;
+    }
+    state.sessions[sessionName] = { participants: {} };
+    const stateMessage = formatMessage('session-created', state);
+
+    notifyClients(stateMessage)
+  };
+
+  formatMessage = (eventType, payload, targetSession) => ({
+    eventType: eventType,
+    payload: payload,
+    targetSession: targetSession,
+  });
+
+  notifyClients = (message) => {
+    wss.clients
+      .forEach(client => {
         client.send(JSON.stringify(message));
-      }
-    });
-};
+      });
+  };
 
-wss.on('connection', handleNewClients);
+  notifyCaller = (message) => {
+    wss.clients
+      .forEach(client => {
+        if (client === _ws) {
+          client.send(JSON.stringify(message));
+        }
+      });
+  };
 
-server.listen(process.env.PORT || 8081, () => {
-  console.log(`Server (${server.address().address}) running on port ${server.address().port}`);
-});
+  wss.on('connection', handleNewClients);
+
+  server.listen(process.env.PORT || 8081, () => {
+    console.log(`Server (${server.address().address}) running on port ${server.address().port}`);
+  });
+
+}
+
+
+const mongoDb = dbClient.initDB()
+  .then(initHandlers)
