68de322fbcedd1c3d5db2117129a433539f3595f
Andrew Gaines <andrew.s.gaines@gmail.com>
2020-01-23 14:13:17 -0700

    Clean

--- go/main.go
+++ go/main.go
@@ -16,7 +16,7 @@ import (
 
 func main() {
 	var port string
-	flag.StringVar(&port, "port", ":8081", "Port on which to host server")
+	flag.StringVar(&port, "port", ":8081", "port on which to host server")
 	flag.Parse()
 
 	dbConf := store.DBConf{
@@ -31,39 +31,29 @@ func main() {
 	if err != nil {
 		log.Fatal("could not create store:", err)
 	}
-	svc := service.New(dstore)
-	//mux := http.NewServeMux()
-	router := mux.NewRouter()
 
-	//static := http.FileServer(http.Dir("../dist/story-points"))
+	svc := service.New(dstore)
+	router := mux.NewRouter()
 
 	router.HandleFunc("/socket", svc.Connect)
 	router.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		path, err := filepath.Abs(r.URL.Path)
 		if err != nil {
-			// if we failed to get the absolute path respond with a 400 bad request
-			// and stop
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
 
-		// prepend the path with the path to the static directory
 		path = filepath.Join("../dist/story-points", path)
 
-		// check whether a file exists at the given path
 		_, err = os.Stat(path)
 		if os.IsNotExist(err) {
-			// file does not exist, serve index.html
 			http.ServeFile(w, r, filepath.Join("../dist/story-points", "index.html"))
 			return
 		} else if err != nil {
-			// if we got an error (that wasn't that the file doesn't exist) stating the
-			// file, return a 500 internal server error and stop
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
-		// otherwise, use http.FileServer to serve the static dir
 		http.FileServer(http.Dir("../dist/story-points")).ServeHTTP(w, r)
 	})
 
