f3690fee995b883d651d6ee09fe82dee04065550
Andrew Gaines <andrew.s.gaines@gmail.com>
2020-01-24 10:23:51 -0700

    Favicon and remove comment

--- src/app/services/socket.service.ts
+++ src/app/services/socket.service.ts
@@ -22,7 +22,6 @@ export class SocketService {
     const wsProtocol = document.location.protocol === 'https:' ? 'wss' : 'ws';
 
     const config = {
-      // url: `ws://localhost:8081/socket`,
       url: `${wsProtocol}://${host}/socket`,
       deserializer: (data) => data,
       openObserver: {
