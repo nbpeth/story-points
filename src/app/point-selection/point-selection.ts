edc59aec30f8180f1612aef068bd2d6942c69a2f
nbpeth <nick.peth@returnpath.com>
2020-01-24 15:27:23 -0700

    all votes are strings in default

--- src/app/point-selection/point-selection.ts
+++ src/app/point-selection/point-selection.ts
@@ -1,11 +1,11 @@
-import {VotingScheme} from '../control-panel/control-panel.component';
-
 export class PointSelection {
   options: any[];
 }
 
 export class DefaultPointSelection implements PointSelection {
-  options = [0, 1, 2, 3, 5, 8, 13, 21, 34, 'Abstain'];
+  options = [
+    '0', '1', '2', '3', '5', '8', '13', '21', '34', 'Abstain'
+  ];
 }
 
 export class FistOfFivePointSelection implements PointSelection {
