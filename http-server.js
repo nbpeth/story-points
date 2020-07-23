11673ca7826dbba1fa03841346dd37902e7e8334
Nicholas Peth <nbpeth@Nicholass-iMac.local>
2020-07-23 16:55:54 -0600

    allow all routes

--- http-server.js
+++ http-server.js
@@ -1,10 +1,14 @@
 const cors = require('cors');
-
+const path = require('path');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+
 app.use(express.static(__dirname + '/dist/story-points'));
+app.get('/*', (_, res) => {
+  res.sendFile(path.join(__dirname + '/dist/story-points/index.html'));
+});
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cors);
