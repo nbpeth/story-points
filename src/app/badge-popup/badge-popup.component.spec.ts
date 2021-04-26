6757fac48d68362417b1defc9fd4613f4a1b7ef5
Nicholas Peth <nick.peth@pieinsurance.com>
2021-04-26 14:03:13 -0500

    track them files

---
+++ src/app/badge-popup/badge-popup.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BadgePopupComponent } from './badge-popup.component';
+
+describe('BadgePopupComponent', () => {
+  let component: BadgePopupComponent;
+  let fixture: ComponentFixture<BadgePopupComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ BadgePopupComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BadgePopupComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/badge-popup/badge-popup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-badge-popup',
+  templateUrl: './badge-popup.component.html',
+  styleUrls: ['./badge-popup.component.scss']
+})
+export class BadgePopupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
