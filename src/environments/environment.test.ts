a3875d030c3a86090f65daf3e7bb8b9fba967302
Nicholas Peth <nick.peth@pieinsurance.com>
2021-07-24 14:31:41 -0500

    test file

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
   host: 'https://story-points-9000-test.herokuapp.com',
+  auth0Domain: 'nameless-meadow-6569.us.auth0.com',
+  auth0ClientId: 'kWz9HeVKHzflLtRHcZDuSijmnH4m22WJ'
 };
